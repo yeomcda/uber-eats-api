f22d3a1e57389f8d9b05f544cfbe1b9bf703da77
yeomcda <>
2021-04-11 13:01:55 +0900

    joi 패키지를 이용한 env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,12 +13,12 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
       ignoreEnvFile: process.env.NODE_ENV === 'prod',
       validationSchema: Joi.object({
-        NODE_ENV: Joi.string().valid('dev', 'prod').required,
-        DB_HOST: Joi.string().required,
-        DB_PORT: Joi.string().required,
-        DB_USERNAME: Joi.string().required,
-        DB_PASSWORD: Joi.string().required,
-        DB_NAME: Joi.string().required,
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
       }),
     }),
     GraphQLModule.forRoot({
